dcb142a09e510fcb7fa4de7c9c8bb8ce196338a7
strwalk <61673527+strwalk@users.noreply.github.com>
2023-09-02 22:52:50 +0900

    chore: add extend screens to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,14 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+    },
   },
   plugins: [],
 };
