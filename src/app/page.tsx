06b4248d31a1ad565258c34f9a205db7fa932c52
strwalk <61673527+strwalk@users.noreply.github.com>
2023-09-03 11:46:55 +0900

    feat: add first view on top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,22 @@
-import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main>
-      <section className="mx-10">
-        <h1>トップ</h1>
-        <nav className="mt-10">
-          <ul className="grid grid-cols-3 gap-10">
-            <li className="bg-green py-3 text-center rounded-lg text-white">
-              <Link href="/profile">プロフィール</Link>
-            </li>
-            <li className="bg-green py-3 text-center rounded-lg text-white">
-              <Link href="/news">ニュース</Link>
-            </li>
-            <li className="bg-green py-3 text-center rounded-lg text-white">
-              <Link href="/blogs">ブログ</Link>
-            </li>
-          </ul>
-        </nav>
+    <main className="relative">
+      <section className="relative h-screen">
+        <Image
+          src="/image-top.jpg"
+          fill={true}
+          style={{ objectFit: 'cover' }}
+          alt="植物の画像"
+          className="z-[-1]"
+        />
+        <section className="relative top-40 sm:absolute sm:top-[40%] sm:left-[10%]">
+          <h1 className="flex flex-col text-center">
+            <span className="text-4xl md:text-6xl lg:text-7xl">青星ヒカリ</span>
+            <span className="mt-1 md:mt-3 md:text-xl">Official Website</span>
+          </h1>
+        </section>
       </section>
     </main>
   );
