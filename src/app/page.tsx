6a91b96f643b4263a7193539847b7e7d3d28f9bd
strwalk <61673527+strwalk@users.noreply.github.com>
2023-09-03 16:39:10 +0900

    feat: add news, profile and blog contents to top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import Link from 'next/link';
 import Image from 'next/image';
+import { HiOutlineChevronRight } from 'react-icons/hi';
+import imageCoffee from '../../public/images/coffee.jpeg';
+import imageSofa from '../../public/images/sofa.jpeg';
+import imageFlower from '../../public/images/flower.jpeg';
 
 export default function Home() {
   return (
     <main className="relative">
       <section className="relative h-screen">
         <Image
-          src="/image-top.jpg"
+          src="/images/top.jpg"
           fill={true}
           style={{ objectFit: 'cover' }}
           alt="植物の画像"
@@ -16,6 +21,69 @@ export default function Home() {
             <span className="text-4xl md:text-6xl lg:text-7xl">青星ヒカリ</span>
             <span className="mt-1 md:mt-3 md:text-xl">Official Website</span>
           </h1>
+        </section>
+      </section>
+      <section className="px-10">
+        <section className="grid grid-cols-1 md:grid-cols-2 justify-center gap-6 py-16">
+          <section className="flex justify-center items-center">
+            <section>
+              <h2 className="text-4xl text-center">News</h2>
+              <p className="mt-4">青星ヒカリの最新ニュースをお届け</p>
+              <p>今後の予定や、メディア出演情報等を掲載しています</p>
+              <section className="mt-10 flex justify-center">
+                <Link
+                  href="/news"
+                  className="flex justify-center gap-2 items-center px-10 py-2.5 text-center rounded-md shadow bg-green text-white hover:bg-green-light hover:text-black"
+                >
+                  <span className="text-lg">ニュース一覧</span>
+                  <HiOutlineChevronRight />
+                </Link>
+              </section>
+            </section>
+          </section>
+          <section className="px-8 md:px-0">
+            <Image src={imageCoffee} alt="コーヒーの画像" />
+          </section>
+        </section>
+        <section className="grid grid-cols-1 md:grid-cols-2 justify-center gap-6 py-16 bg-green-light">
+          <section className="px-8 md:px-0">
+            <Image src={imageSofa} alt="家具の画像" />
+          </section>
+          <section className="flex justify-center items-center">
+            <section>
+              <h2 className="text-4xl text-center">Profile</h2>
+              <p className="mt-4">青星ヒカリのプロフィールや来歴はこちらから</p>
+              <section className="mt-10 flex justify-center">
+                <Link
+                  href="/profile"
+                  className="flex justify-center gap-2 items-center px-10 py-2.5 text-center rounded-md shadow bg-green text-white hover:bg-green-light hover:text-black"
+                >
+                  <span className="text-lg">プロフィール</span>
+                  <HiOutlineChevronRight />
+                </Link>
+              </section>
+            </section>
+          </section>
+        </section>
+        <section className="grid grid-cols-1 md:grid-cols-2 justify-center gap-6 py-16">
+          <section className="flex justify-center items-center">
+            <section>
+              <h2 className="text-4xl text-center">Blog</h2>
+              <p className="mt-4">青星ヒカリのブログを読む</p>
+              <section className="mt-10 flex justify-center">
+                <Link
+                  href="/blogs"
+                  className="flex justify-center gap-2 items-center px-10 py-2.5 text-center rounded-md shadow bg-green text-white hover:bg-green-light hover:text-black"
+                >
+                  <span className="text-lg">ブログ記事一覧</span>
+                  <HiOutlineChevronRight />
+                </Link>
+              </section>
+            </section>
+          </section>
+          <section className="px-8 md:px-0">
+            <Image src={imageFlower} alt="花束の画像" />
+          </section>
         </section>
       </section>
     </main>
