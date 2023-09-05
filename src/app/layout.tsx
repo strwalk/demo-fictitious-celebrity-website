9d49fd19f59dd0ca51173e34d2c5e8b8c1373556
strwalk <61673527+strwalk@users.noreply.github.com>
2023-09-06 00:02:57 +0900

    fix: remove google fonts from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import Header from './_components/header';
 import Footer from './_components/footer';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} relative min-h-screen`}>
+      <body className="relative min-h-screen">
         <Header />
         {children}
         <Footer />
