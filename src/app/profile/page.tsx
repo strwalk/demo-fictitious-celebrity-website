04540080611114b00447f67f4ea6db8e39f44b1e
strwalk <61673527+strwalk@users.noreply.github.com>
2023-09-09 16:59:29 +0900

    refactor: image component and resolve eslint errors

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import imageLion from '../../../public/images/lion.jpeg';
 
 export default function Profile() {
   return (
@@ -9,7 +8,14 @@ export default function Profile() {
           <section>
             <h1 className="text-3xl text-center">Profile</h1>
             <section className="mt-5 flex justify-center">
-              <Image src={imageLion} priority={true} alt="プロフィール画像" />
+              <Image
+                src="/images/lion.jpeg"
+                width={580}
+                height={580}
+                priority={true}
+                alt="プロフィール画像"
+                className="w-[33rem] h-[20rem] object-cover object-right-top"
+              />
             </section>
             <section className="flex flex-wrap gap-x-1 items-end mt-10">
               <h2 className="text-3xl">青星ヒカリ</h2>
@@ -23,12 +29,14 @@ export default function Profile() {
               <li>職業：タレント</li>
             </ul>
             <ul className="mt-6 text-sm sm:text-base leading-7 sm:leading-7">
+              {/* eslint-disable no-irregular-whitespace */}
               <li>2000年　大手芸能事務所にスカウトされ、デビュー</li>
               <li>2005年　写真集「Light」を出版</li>
               <li>2002年　レギュラー番組「ブルーなStar」がスタート</li>
               <li>2020年　エッセイ本「Sirius」を出版</li>
               <li>2020年　ドラマ「universe」に出演</li>
               <li>2020年　若手新人女優賞を受賞</li>
+              {/* eslint-disable no-irregular-whitespace */}
             </ul>
           </section>
         </section>
