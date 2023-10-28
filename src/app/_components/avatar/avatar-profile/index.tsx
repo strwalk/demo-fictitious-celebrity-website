49f606b6ae467fa344f2943c8ff2b7af0876f32d
strwalk <61673527+strwalk@users.noreply.github.com>
2023-10-28 14:59:44 +0900

    fix: avatar profile component

--- src/app/_components/avatar/avatar-profile/index.tsx
+++ src/app/_components/avatar/avatar-profile/index.tsx
@@ -3,9 +3,11 @@ import AvatarIcon from '../avatar-icon';
 
 export default function AvatarProfile() {
   return (
-    <section className="flex gap-5 items-center bg-gray py-3 px-4">
-      <AvatarIcon size="big" hasLink={true} />
-      <section>
+    <section className="flex gap-3 md:gap-5 items-center bg-gray py-3 px-3 sm:px-4">
+      <section className="basis-36 sm:basis-auto flex justify-center">
+        <AvatarIcon size="big" hasLink={true} />
+      </section>
+      <section className="basis-auto">
         <p>{profileName}</p>
         <p className="text-xs mt-1">
           2005年10月10日生まれ。大手芸能事務所所属のタレント
