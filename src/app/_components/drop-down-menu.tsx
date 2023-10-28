e31221e1f1ad2e8c390872dd0aee3cb6e944cbf5
strwalk <61673527+strwalk@users.noreply.github.com>
2023-10-28 09:35:31 +0900

    feat: create drop down menu component

---
+++ src/app/_components/drop-down-menu.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+} from '@nextui-org/dropdown';
+import { Button } from '@nextui-org/button';
+
+export default function DropDownMenu() {
+  const router = useRouter();
+
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        <Button variant="bordered">Menu</Button>
+      </DropdownTrigger>
+      <DropdownMenu
+        aria-label="menu"
+        onAction={(key) => router.push(key.toString())}
+      >
+        <DropdownItem key="/">Top</DropdownItem>
+        <DropdownItem key="/news">News</DropdownItem>
+        <DropdownItem key="/profile">Profile</DropdownItem>
+        <DropdownItem key="/blogs">Blog</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
